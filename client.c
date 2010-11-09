a1312d489d59a9b35eae73d6954e8eb796b8d628
Dalthon <Dalthon@localhost>
2010-11-09 10:42:15 -0200

    Added support of handling Unix signal QUIT on realservers test

--- client.c
+++ client.c
@@ -10,38 +10,26 @@
 // IP:    unsigned int   (4 bytes)
 
 // ------- Function Prototypes --------
-void error(char *msg);
-string recv_string(int sock);
-void send_string(int sock, string str);
-void free_string(string str)
+// void error(char *msg);
+// string recv_string(int sock);
+// void send_string(int sock, string str);
+// void free_string(string str)
 
 // ------- Commands --------
-void cmd_list_servers(int sock);
-void cmd_conect(int sock);
-void cmd_send_message(int sock);
-void cmd_heartbeat(int sock);
-void cmd_list_users(int sock);
-void cmd_echo(int sock);
-void cmd_unknown(int sock);
-void cmd_receive_message(int sock);
+// void cmd_list_servers(int sock);
+// void cmd_conect(int sock);
+// void cmd_send_message(int sock);
+// void cmd_heartbeat(int sock);
+// void cmd_list_users(int sock);
+// void cmd_echo(int sock);
+// void cmd_unknown(int sock);
+// void cmd_receive_message(int sock);
+
+void sigquit(){ exit(0); }
 
 int main(int argc, char *argv[])
 {
-  int sockfd, newsockfd, portno, clilen;
-  struct sockaddr_in serv_addr, cli_addr;
-  pthread_t chld_thr;
-  void *arg;
+  signal(SIGQUIT, sigquit); // Waiting to be killed (kill -QUIT <pid>)
 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0)
-      error("ERROR opening socket");
-  bzero((char *) &serv_addr, sizeof(serv_addr));
-  portno = atoi(argv[1]);
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = INADDR_ANY;
-  serv_addr.sin_port = htons(portno);
-  if (bind(sockfd, (struct sockaddr *) &serv_addr,
-          sizeof(serv_addr)) < 0)
-          error("ERROR on binding");
-  listen(sockfd, 5);  
+  return 0;
 }
